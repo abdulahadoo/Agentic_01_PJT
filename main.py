6612347f2538e8a697205f75d8a883bed4ec4694
Ab ahad <164404456+abdulahadoo@users.noreply.github.com>
2025-05-26 14:00:28 +0500

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def main():
-    print("Hello code with AD")
+import streamlit as st
 
+def main():
+    st.write("Hello code with AD")
 
 if __name__ == "__main__":
     main()
+
+print("Pakistan Hamsha zindabad/")
+ 
 
